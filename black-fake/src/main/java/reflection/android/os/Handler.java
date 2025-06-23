9af09beaeca52ddea180c2d6b564fa5cd5fc16b6
ZiTanIOI <t076nl3456832@163.com>
2025-06-23 11:21:23 +0800

    Delete black-fake directory

--- black-fake/src/main/java/reflection/android/os/Handler.java
+++
@@ -1,9 +0,0 @@
-package reflection.android.os;
-
-import reflection.MirrorReflection;
-
-public class Handler {
-    public static final MirrorReflection REF = MirrorReflection.on(android.os.Handler.class);
-
-    public static MirrorReflection.FieldWrapper<android.os.Handler.Callback> mCallback = REF.field("mCallback");
-}
